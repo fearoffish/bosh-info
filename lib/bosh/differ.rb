d25d4d326939e41d8208f66ddd7a5c2103f7f622
Jamie van Dyke <jamie@fearoffish.com>
2013-08-21 17:12:22 +0100

    initial commit

---
+++ lib/bosh/differ.rb
@@ -0,0 +1,7 @@
+require "bosh/differ/version"
+
+module Bosh
+  module Differ
+    # Your code goes here...
+  end
+end
---
+++ lib/bosh/differ/version.rb
@@ -0,0 +1,5 @@
+module Bosh
+  module Differ
+    VERSION = "0.0.1"
+  end
+end
